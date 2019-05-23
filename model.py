57e14dbbaa73648d5f4121e6f35cd3a829f91d88
sally14 <salome.do@ensae.fr>
2019-05-23 18:24:13 +0200

    refactored

---
+++ model.py
@@ -0,0 +1,167 @@
+"""
+
+"""
+
+import tensorflow as tf
+
+from submodels import CharacterEmbedding, LSTMSequenceEmbedding
+from submodels import EncoderLayer
+from submodels import FinishDense
+from submodels import LossConstructor
+
+
+def model_fn(features, labels, mode, params):
+    # Get the params
+    # Hardware config
+    gpu_train = params['gpu_train']
+
+    # Learning params
+    batch_size = params["batch_size"]
+    dropout = params["dropout"]
+    lr = params["learning_rate"]
+    optim = params["optimizer"]
+
+    # Embedding params
+    vocab_size = params["vocab_size"]
+    embedding_size = params['vocab_size']
+
+    # LSTM params
+    num_LSTM_layers = params["num_LSTM_layers"]
+    hidden_size_LSTM = params["hidden_size_LSTM"]
+
+    # Char embedding parameters
+    add_chars_emb = params['add_char_emb']
+    if add_chars_emb:
+        num_layers_chars = params['num_layers_char']
+        hidden_size_chars = params['hidden_size_chars']
+        nb_chars = params['nb_chars']
+
+    # TODO : positionnal embedding parameters
+
+    # Encoder parameters
+    num_heads = params['num_heads']
+
+    # Finish parameters
+    deepness_finish = params['deepness_finish']
+    activation_finish = params['activation_finish']
+    inter_size = params['intern_size']
+
+    # Loss parameters
+    weighted_loss = params['weighted_loss']
+    frequencies = params['frequencies']
+
+    # Get the inputs
+    (input_sentences, input_char), seq_length = features
+    training = mode == tf.estimator.ModeKeys.TRAIN
+
+    # First embed words
+    W2V_embedding = tf.get_variable(
+                        name='W2V_embedding',
+                        shape=[vocab_size, embedding_size],
+                        dtype=tf.float32,
+                        trainable=True
+                        )
+    word_embeddings = tf.nn.embedding_lookup(
+                        W2V_embedding,
+                        input_sentences,
+                        name="W2V_embedded"
+                        )
+
+    # Depending on parameters, do a character embedding
+    if add_chars_emb:
+        char_emb = CharacterEmbedding(
+            num_layers=num_layers_chars,
+            hidden_size_chars=hidden_size_chars,
+            nb_chars=nb_chars,
+            gpu_train=gpu_train,
+            is_training=training,
+            dropout=dropout)
+        chars_embedded = char_emb(input_char=input_char)
+        word_embeddings = tf.concat([word_embeddings, chars_embedded], axis=-1)
+
+    word_embeddings = tf.nn.dropout(word_embeddings, dropout)
+    embedding_shape = tf.shape(word_embeddings)
+
+    # Encoder phase :
+    sample_encoder_layer = EncoderLayer(embedding_shape[-1],
+                                        num_heads, 2048)
+    out_sequence = sample_encoder_layer(word_embeddings, False, None)
+
+    # LSTM sequence embedding
+    seq_emb = LSTMSequenceEmbedding(
+                num_layers=num_LSTM_layers,
+                dim_hidden_state=hidden_size_LSTM,
+                gpu_train=gpu_train,
+                is_training=training,
+                dropout=dropout)
+    output = seq_emb(out_sequence)
+
+    dense = FinishDense(
+                deepness_finish=deepness_finish,
+                activation_finish=activation_finish,
+                intern_size=inter_size,
+                is_training=training,
+                dropout=dropout)
+    logits = dense(output, vocab_size)
+
+    labels_pred = tf.math.argmax(logits, axis=1)
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        top_labels = tf.math.top_k(logits, k=10).indices
+        predictions = {"label": labels_pred, "top_k": top_labels}
+        export_outputs = {
+            "predictions": tf.estimator.export.PredictOutput(predictions)
+        }
+        return tf.estimator.EstimatorSpec(
+            mode, predictions=predictions, export_outputs=export_outputs
+        )
+    else:
+        loss = LossConstructor(
+                vocab_size,
+                weighted_loss=weighted_loss,
+                frequencies=frequencies)
+        loss_mean, weigthed = loss(logits, labels)
+        tvars = tf.trainable_variables()
+        if weighted_loss:
+            grads, _ = tf.clip_by_global_norm(
+                        tf.gradients(weigthed, tvars),
+                        10)
+        else:
+            grads, _ = tf.clip_by_global_norm(
+                        tf.gradients(loss_mean, tvars),
+                        10)
+        ppxl = tf.exp(loss_mean)
+
+        acc = tf.metrics.accuracy(labels=labels,
+                                  predictions=labels_pred,
+                                  name='accuracy')
+
+        metrics = {'accuracy': acc}
+        # For Tensorboard
+        for k, v in metrics.items():
+            # v[1] is the update op of the metrics object
+            tf.summary.scalar(k, v[1])
+        tf.summary.scalar('perplexity', ppxl)
+        if mode == tf.estimator.ModeKeys.EVAL:
+            return tf.estimator.EstimatorSpec(
+                mode, loss=loss_mean, eval_metric_ops=metrics
+            )
+        # 5. Define EstimatorSpecs for TRAIN
+        elif mode == tf.estimator.ModeKeys.TRAIN:
+
+            if optim == "adam":
+                opt = tf.train.AdamOptimizer(lr)
+            elif optim == "adagrad":
+                opt = tf.train.AdagradOptimizer(lr)
+            elif optim == "sgd":
+                opt = tf.train.GradientDescentOptimizer(lr)
+            elif optim == "rmsprop":
+                opt = tf.train.RMSPropOptimizer(lr)
+            
+            train_op = opt.apply_gradients(
+                        zip(grads, tvars),
+                        global_step=tf.train.get_or_create_global_step())
+
+            return tf.estimator.EstimatorSpec(
+                mode, loss=loss_mean, train_op=train_op
+            )
---
+++ submodels.py
@@ -0,0 +1,283 @@
+"""
+Submodels for the tensorflow graph
+"""
+import tensorflow as tf
+
+
+def point_wise_feed_forward_network(d_model, dff):
+    return tf.keras.Sequential([
+          tf.keras.layers.Dense(dff, activation='relu'),  # (batch_size, seq_len, dff)
+          tf.keras.layers.Dense(d_model)  # (batch_size, seq_len, d_model)
+      ])
+
+def scaled_dot_product_attention(q, k, v, mask):
+    """Calculate the attention weights.
+      q, k, v must have matching leading dimensions.
+      k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
+      The mask has different shapes depending on its type(padding or look ahead) 
+      but it must be broadcastable for addition.
+
+      Args:
+        q: query shape == (..., seq_len_q, depth)
+        k: key shape == (..., seq_len_k, depth)
+        v: value shape == (..., seq_len_v, depth_v)
+        mask: Float tensor with shape broadcastable 
+              to (..., seq_len_q, seq_len_k). Defaults to None.
+
+      Returns:
+        output, attention_weights
+    """
+
+    matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
+
+    # scale matmul_qk
+    dk = tf.cast(tf.shape(k)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    # add the mask to the scaled tensor.
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+        # softmax is normalized on the last axis (seq_len_k) so that the scores
+        # add up to 1.
+    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)  # (..., seq_len_q, seq_len_k)
+
+    output = tf.matmul(attention_weights, v)  # (..., seq_len_v, depth_v)
+
+    return output, attention_weights
+
+
+class CharacterEmbedding(tf.keras.layers.Layer):
+    def __init__(self,
+                 num_layers,
+                 dim_hidden_state,
+                 nb_chars,
+                 gpu_train=True,
+                 is_training=True,
+                 dropout=0):
+        super(CharacterEmbedding, self).__init__()
+        self.num_layers = num_layers
+        self.dim_hidden_state = dim_hidden_state
+        self.nb_chars = nb_chars
+        if gpu_train:
+            self.RNN = tf.contrib.cudnn_rnn.CudnnLSTM(
+                            self.num_layers,
+                            self.dim_hidden_state,
+                            direction="bidirectional"
+                        )
+        else:
+            self.RNN = tf.contrib.rnn.BasicLSTMCell(
+                            self.dim_hidden_state,
+                            forget_bias=0.0,
+                            state_is_tuple=True,
+                            reuse=not is_training)
+        if is_training:
+            self.RNN = tf.contrib.rnn.DropoutWrapper(
+                            self.RNN,
+                            output_keep_prob=dropout)
+
+    def call(self, input_char):
+        self.char_embedding = tf.get_variable(
+                                name="char_embedding",
+                                dtype=tf.float32,
+                                shape=[self.nb_chars, self.dim_hidden_state],
+                                )
+        self.char_embedded = tf.nn.embedding_lookup(
+                                self.char_embedding,
+                                input_char,
+                                name="char_embedded"
+                                )
+        # put the time dimension on axis=1
+        s = tf.shape(self.char_embedded)
+        self.char_embedded = tf.reshape(
+                                self.char_embedded,
+                                shape=[s[0] * s[1], s[-2], self.dim_hidden_state]
+                                )
+
+        outputs, output_states = self.RNN(tf.transpose(self.char_embedded, [1, 0, 2]))
+        # tf.transpose = LSTM handles differently time axis
+        self.output_fw = output_states[0][1]
+        self.output_bw = output_states[1][1]
+        output = tf.concat([self.output_fw, self.output_bw], axis=-1)
+        # shape = (batch size, max sentence length, char hidden size)
+        self.output = tf.reshape(output, shape=[s[0], s[1], 2 * self.dim_hidden_state])
+        return output
+
+
+class LSTMSequenceEmbedding(tf.keras.layers.Layer):
+    def __init__(self,
+                 num_layers,
+                 dim_hidden_state,
+                 gpu_train=True,
+                 is_training=True,
+                 dropout=0):
+        super(LSTMSequenceEmbedding, self).__init__()
+        self.num_layers = num_layers
+        self.dim_hidden_state = dim_hidden_state
+        if gpu_train:
+            self.RNN = tf.contrib.cudnn_rnn.CudnnLSTM(
+                            self.num_layers,
+                            self.dim_hidden_state,
+                            direction="bidirectional"
+                        )
+        else:
+            self.RNN = tf.contrib.rnn.BasicLSTMCell(
+                            self.dim_hidden_state,
+                            forget_bias=0.0,
+                            state_is_tuple=True,
+                            reuse=not is_training)
+        if is_training:
+            self.RNN = tf.contrib.rnn.DropoutWrapper(
+                            self.RNN,
+                            output_keep_prob=dropout)
+
+    def call(self, input_sequence):
+        outputs, output_states = self.RNN(tf.transpose(input_sequence, [1, 0, 2]))
+        # tf.transpose = LSTM handles differently time axis
+        self.output_fw = output_states[0][1]
+        self.output_bw = output_states[1][1]
+        self.output = tf.concat([self.output_fw, self.output_bw], axis=-1)
+        return self.output
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.d_model = d_model
+
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.wq = tf.keras.layers.Dense(d_model)
+        self.wk = tf.keras.layers.Dense(d_model)
+        self.wv = tf.keras.layers.Dense(d_model)
+
+        self.dense = tf.keras.layers.Dense(d_model)
+
+    def split_heads(self, x, batch_size):
+        """Split the last dimension into (num_heads, depth).
+        Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
+        """
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, v, k, q, mask):
+        batch_size = tf.shape(q)[0]
+
+        q = self.wq(q)  # (batch_size, seq_len, d_model)
+        k = self.wk(k)  # (batch_size, seq_len, d_model)
+        v = self.wv(v)  # (batch_size, seq_len, d_model)
+
+        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
+        k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len_k, depth)
+        v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len_v, depth)
+
+        # scaled_attention.shape == (batch_size, num_heads, seq_len_v, depth)
+        # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
+        scaled_attention, attention_weights = scaled_dot_product_attention(
+            q, k, v, mask)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])  # (batch_size, seq_len_v, num_heads, depth)
+
+        concat_attention = tf.reshape(scaled_attention, 
+                                      (batch_size, -1, self.d_model))  # (batch_size, seq_len_v, d_model)
+
+        output = self.dense(concat_attention)  # (batch_size, seq_len_v, d_model)
+
+        return output, attention_weights
+
+
+class EncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, d_model, num_heads, dff, rate=0.1):
+        super(EncoderLayer, self).__init__()
+
+        self.mha = MultiHeadAttention(d_model, num_heads)
+        self.ffn = point_wise_feed_forward_network(d_model, dff)
+
+        self.layernorm1 = tf.contrib.layers.layer_norm
+        self.layernorm2 = tf.contrib.layers.layer_norm
+
+        self.dropout1 = tf.keras.layers.Dropout(rate)
+        self.dropout2 = tf.keras.layers.Dropout(rate)
+
+    def call(self, x, training, mask):
+
+        attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
+
+        ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layernorm2(out1 + ffn_output)  # (batch_size, input_seq_len, d_model)
+
+        return out2
+
+
+class FinishDense(tf.keras.layers.Layer):
+    def __init__(self,
+                 deepness_finish,
+                 activation_finish=tf.nn.leaky_relu,
+                 intern_size=300,
+                 is_training=True,
+                 dropout=0):
+        super(FinishDense, self).__init__()
+        self.deepness_finish = deepness_finish
+        self.activation_finish = activation_finish
+        self.is_training = is_training
+        self.dropout = dropout
+
+    def call(self, input, vocab_size):
+        nb_dense = self.deepness_finish
+        for i in range(nb_dense-1):
+            input = tf.layers.dense(
+                        input,
+                        self.intern_size,
+                        activation=self.activation_finish)
+        output = tf.layers.dense(
+                        input,
+                        vocab_size)
+        return output
+
+
+class LossConstructor(tf.keras.layers.Layer):
+    def __init__(self,
+                 vocab_size,
+                 weighted_loss=False,
+                 frequencies=None):
+        super(LossConstructor, self).__init__()
+        """
+        frequencies must be a tensor [vocab_size, 1] indicating the frequencies of 
+        each word over the training corpus
+        """
+        self.weighted_loss = weighted_loss
+        self.frequencies = frequencies
+        self.vocab_size = vocab_size
+        if self.weighted_loss:
+            self.frequencies = tf.get_variable(
+                                self.frequencies,
+                                name='frequencies',
+                                shape=[self.vocab_size, 1],
+                                dtype=tf.float32,
+                                trainable=False
+                                )
+            self.inv_freq = 1/self.frequencies
+
+    def call(self, logits, labels):
+        labels_one_hot = tf.squeeze(tf.one_hot(labels, 
+                                    depth=self.vocab_size))
+        if self.weighted_loss:
+            freq_weights = tf.nn.embedding_lookup(
+                                self.inv_freq,
+                                labels,
+                                name="freq_weights"
+                                )
+            losses = tf.losses.softmax_cross_entropy(labels_one_hot, logits)
+            self.weighted_loss = tf.losses.compute_weighted_loss(losses, freq_weights)
+        else:
+            self.weighted_loss = None
+        loss = tf.losses.softmax_cross_entropy(labels_one_hot, logits)
+        self.loss = tf.reduce_mean(loss)
+        return self.loss, self.weighted_loss
+
