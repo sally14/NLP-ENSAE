ca11c0c9100faf08acfd3739b4f1f1426563c2e1
sally14 <salome.do@ensae.fr>
2019-05-23 20:14:52 +0200

    Finalized refactoring

---
+++ reader.py
@@ -0,0 +1,202 @@
+"""
+Reader functions to input data in tf.estimator
+"""
+import os
+import tensorflow as tf
+from glob import glob
+
+
+def lower_tensor(tens):
+    lower = tf.py_func(
+        lambda x: x.lower(), tens, tf.string, stateful=False
+    )
+    return lower
+
+
+def decode_tensor(tens):
+    lower = tf.py_func(
+        lambda x: x.lower(), tens, tf.string, stateful=False
+    )
+    return lower
+
+
+def extract_char(token, default_value="<pad_char>"):
+    # Split characters
+    out = tf.string_split(token, delimiter="")
+    # Convert to Dense tensor, filling with default value
+    out = tf.sparse_tensor_to_dense(out, default_value=default_value)
+    return out
+
+
+def extract_char_V2(tens):
+    out = tens.map(lambda x: extract_char(x))
+    return out
+
+
+# INPUT FUNCTIONS
+def input_fn_gen(mode, params, **kwargs):
+    """
+    input_fn
+        Generates a dataset to be fed to the tf.Estimator model
+        As the input_fn function cannot take arguments in tf.Estimator,
+        it is assumed that parameters are fixed while running the script,
+        declared outside the function. One could have curryfied the
+        function in order to be cleaner, but I have choosen to keep
+        it simple for readability.
+    Args
+        None
+
+    Returns
+        dataset
+            A tf.dataset object which yields
+            ((dataset_words, dataset_chars), dataset_labels)
+            where :
+            - dataset_words is a [batch_size, max_l] tensor (max_l= maximum sentence
+            length over the current batch). Words are NOT embedded before entering
+            the model, their are encoded as their index in the vocabulary.
+            - dataset_chars is a [batch_size, max_l, max_l_char] tensor. (max_l 
+            defined as before, max_l_char = max length of a word in terms
+            of characters over the current batch). Chars are embedded as their 
+            index in the character vocabulary.
+            - dataset_labels is a [batch_size, max_l] tensor containing
+            the labels ids for each words for each sentence in the batch.
+    """
+    # Get the important parameters to generate datasets
+    buffer_size = params["buffer_size"]
+    batch_size = params['batch_size']
+
+    def input_fn():
+        # Get the files list:
+        if mode == 'train':
+            sent_files = glob(os.path.join(params["filepath"], "*.train.txt.sents"))
+            label_files = glob(os.path.join(params["filepath"], "*.train.txt.labels"))
+            # Tensorflow TextLineDataset reads files, file per file,
+            # line per line, and outputs tensors containing the string
+            # for each line.
+            sent_lines = tf.data.TextLineDataset(
+                sent_files, buffer_size=buffer_size
+            )
+            label_lines = tf.data.TextLineDataset(
+                label_files, buffer_size=buffer_size
+            )
+            repeat_ = params["n_epochs"]
+
+        elif mode == 'eval':
+            sent_files = glob(os.path.join(params["filepath"], "*.test.txt.sents"))
+            label_files = glob(os.path.join(params["filepath"], "*.test.txt.labels"))
+            # Tensorflow TextLineDataset reads files, file per file,
+            # line per line, and outputs tensors containing the string
+            # for each line.
+            sent_lines = tf.data.TextLineDataset(
+                sent_files, buffer_size=buffer_size
+            )
+            label_lines = tf.data.TextLineDataset(
+                label_files, buffer_size=buffer_size
+            )
+            repeat_ = 1
+        elif mode == 'predict':
+            sent = kwargs['sent']
+            sent_lines = tf.data.Dataset.from_tensor_slices(
+                            tf.constant([sent],
+                                        dtype=tf.string))
+            label_lines = tf.data.Dataset.from_tensor_slices(
+                            tf.constant(['useless'],
+                                        dtype=tf.string))
+            repeat_ = 1
+
+        else:
+            print('mode error')
+
+        cores = tf.data.experimental.AUTOTUNE
+        # WARNING : custom decode_tensor & lower_tensor functions
+        # might have reproductibility issues using tf.save because of pyfunc
+        # Encoding forcing through unidecode
+        decoded_tokens = sent_lines.map(
+            lambda token: decode_tensor([token]), num_parallel_calls=nb_cores
+        )
+        # Lower words before indexing for tokens,
+        # for compatibility with word embeddings
+        lowered_tokens = decoded_tokens.map(
+            lambda token: lower_tensor([token])
+        )
+        # Split tokens along whitespace
+        dataset_tokens = lowered_tokens.map(
+            lambda string: tf.string_split([string], delimiter=" ").values,
+            num_parallel_calls=cores,
+        )
+        # No lowering for characters to keep maximum information,
+        # split a first time along whitespaces
+        decoded_chars = decoded_tokens.map(
+            lambda string: tf.string_split([string], delimiter=" ").values,
+            num_parallel_calls=cores,
+        )
+
+        dataset_labels = label_lines.map(
+            lambda string: tf.string_split([string], delimiter=" ").values,
+            num_parallel_calls=cores,
+        )
+        dataset_chars = decoded_chars.apply(extract_char_V2)
+
+        # Vocabulary, label vocab, char vocab
+        words = tf.contrib.lookup.index_table_from_file(
+            params["word_emb_vocab"], num_oov_buckets=1
+        )
+        chars = tf.contrib.lookup.index_table_from_file(
+            params["char_vocab"], num_oov_buckets=1
+        )
+
+        # Embed words, labels, chars with their indexes in vocabularies.
+        dataset_tokens = dataset_tokens.map(
+            lambda tokens: words.lookup(tokens), num_parallel_calls=cores
+        )
+        dataset_seq_length = dataset_tokens.map(
+            lambda tokens: tf.size(tokens), num_parallel_calls=cores
+        )
+        dataset_labels = dataset_labels.map(
+            lambda tokens: words.lookup(tokens), num_parallel_calls=cores
+        )
+        dataset_chars = dataset_chars.map(
+            lambda tokens: chars.lookup(tokens), num_parallel_calls=cores
+        )
+
+        # Now needs zipping,shuffling, unzipping (for padding reasons)
+        dataset_input = tf.data.Dataset.zip((dataset_tokens, dataset_chars))
+        dataset = tf.data.Dataset.zip(
+            ((dataset_input, dataset_seq_length), dataset_labels)
+        ).shuffle(buffer_size)
+
+        intermediate = dataset.map(lambda a, b: a)
+        dataset_labels = dataset.map(lambda a, b: b)
+        dataset_input = intermediate.map(lambda a, b: a)
+        dataset_seq_length = intermediate.map(lambda a, b: b)
+
+        padded_shapes = (
+            tf.TensorShape([None]),  # padding the words
+            tf.TensorShape([None, None]),
+        )
+        padding_values = (
+            words.lookup(tf.constant(["<pad_word>"]))[0],
+            chars.lookup(tf.constant(["<pad_char>"]))[0],
+        )
+        dataset_input = dataset_input.padded_batch(
+            batch_size,
+            padded_shapes=padded_shapes,
+            padding_values=padding_values,
+        )
+
+        # padding the characters for each word
+        padded_shapes = tf.TensorShape([None])
+        # arrays of labels padded on the right with <pad>
+
+        dataset_seq_length = dataset_seq_length.batch(batch_size)
+
+        dataset_labels = dataset_labels.batch(batch_size)
+
+        dataset = tf.data.Dataset.zip(
+            ((dataset_input, dataset_seq_length), dataset_labels)
+        )
+
+        # Shuffle the dataset and repeat:
+        dataset = dataset.repeat(repeat_)
+        return dataset
+    return input_fn()
---
+++ train.py
@@ -0,0 +1,156 @@
+"""
+                                Training Language Model
+
+This script trains/train and evaluate a word-level
+language model, implementation with Tensorflow 1.13, tf.Estimator API
+
+Usage:
+  train.py <filepath> <log_dir>
+  [--gpu_train=<gpu>] [--mode=<md>]
+  [--embedding_size=<ems>] [--char_emb_size=<em>]
+  [--num_LSTM_layers=<nmlstm>] [--num_layers_char=<nmchar>]
+  [--hidden_size_char=<hsc>]  [--hidden_size_LSTM=<hsn>]
+  [--add_char_emb=<add>] [--num_heads=<nmh>]
+  [--dropout=<dp>]
+  [--learning_rate=<lr>] [--batch_size=<bs>]
+  [--n_epochs=<ne>] [--optimizer=<op>] [--checkpoints=<ckpt>]
+  [--buffer_size=<bfs>]
+  [--deepness_finish=<dpsf>] [--activation_finish=<actf>]
+  [--intern_size=<intf>] [--weighted_loss=<wgth>]
+
+Options:
+  -h --help
+  --version
+  --filepath                File directories, where the training, and eventually testing files are stored, files to predict in case mode=predict
+  --log_dir                 Logs directory, where the checkpoints will be stored
+  --gpu_train=<gpu>         Boolean, True is using a gpu. (Requires tensorflow-gpu) [default: True]
+  --mode=<md>               The tensorflow mode, in ['train', 'train_eval', 'eval'] [default: train_eval]
+  --embedding_size=<ems>    The word embedding dimension [default: 300]
+  --char_emb_size=<em>      The chosen character embedding size [default: 100]
+  --num_LSTM_layers         The number of LSTM units [default: 2]
+  --num_layers_char         The number of LSTM units for char embedding [default: 2]
+  --hidden_size_char=<hsc>  The hidden size of the character embedding Bi-LSTM. [default: 25]
+  --hidden_size_LSTM=<hsn>  The hidden size of the LSTM. [default: 600]
+  --add_char_emb=<add>      Boolean, True if char embedding is needed [default: False]
+  --num_heads=<nmh>         The number of head for the attention encoder layer [default: 10]
+  --dropout=<dp>            The dropout value. [default: 0.5]
+  --learning_rate=<lr>      The learning rate for the optimizer [default: 1e-3]
+  --batch_size=<bs>         The batch size for the training [default: 24]
+  --n_epochs=<ne>           Number of epochs to train the network on [default: 100]
+  --optimizer=<op>          The chosen tf optimizer, in ['sgd', 'adam', 'adagrad', 'rmsprop'] [default: sgd]
+  --checkpoints=<ckpt>      Save checkpoints every ckpt steps [default:5000]
+  --buffer_size=<bfs>       Buffer size for shuffling  [default: 500]
+  --deepness_finish=<dpsf>  The layer depth for the dense finish [default: 2]
+  --activation_finish=<actf> The activation function for the dense finish [default: leaky_relu]
+  --intern_size=<intf>      The internal dimension of finish dense layers [default: 3000]
+  --weighted_loss=<wgth>    Boolean, indicates if loss must be weighted or not [default: True]
+"""
+
+
+import os
+import logging
+
+import tensorflow as tf
+from docopt import docopt
+from functools import partial
+from numpy import genfromtxt
+
+from utils import generate_dataset, create_full_vocab
+from reader import input_fn_gen
+from model import model_fn
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+
+
+if __name__ == "__main__":
+    # Get the arguments
+    args = docopt(__doc__, version="0.3")
+    print(args)
+    # Transform it into a params dic
+    params = {}
+    for k in args.keys():
+        k2 = k.replace("<", "").replace(">", "").replace("-", "")
+        try:  # Convert strings to int or floats when required
+            params[k2] = int(args[k])
+        except:
+            try:
+                params[k2] = float(args[k])
+            except:
+                params[k2] = args[k]
+
+    # Checking if logdir already has the emb files:
+    params['embedding_path'] = os.path.join(params['log_dir'], 'embedding')
+    if not os.path.isdir(params['log_dir']):
+        os.makedirs(params['log_dir'])
+    if not os.path.isdir(params['embedding_path']):
+        os.makedirs(params['embedding_path'])
+        create_full_vocab(params['filepath'], params['embedding_path'])
+
+    # Create dataset files if not already done:
+    if not os.path.isdir(params['log_dir']):
+        params['data_path'] = os.path.join(params['log_dir'], 'dataset_lm')
+        generate_dataset(params['filepath'], params['log_dir'])
+
+    # Path to vocabs for indexing
+    params["word_emb_vocab"] = os.path.join(
+        params["embedding_path"], "vocab.txt"
+    )
+    params["char_vocab"] = os.path.join(
+        params["embedding_path"], "chars_vocab.txt"
+    )
+
+    # Get nb_chars, nb_labels, & nb_words for params (used in model):
+    with open(params["word_emb_vocab"], "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        params["vocab_size"] = len(lines)
+        params["max_len_sent"] = max(map(len, lines))
+    with open(params["char_vocab"], "r", encoding="utf-8") as f:
+        params["nb_chars"] = len(f.readlines())
+
+    params['frequencies'] = genfromtxt(os.path.join(
+                                  params['embedding_path'],
+                                  'freq.txt'),
+                                delimiter="\t")
+    # Create input functions
+    input_fn = partial(input_fn_gen, mode='train', params='params')
+    input_eval = partial(input_fn_gen, mode='eval', params='params')
+
+    # Session config for tensorflow
+    config = tf.ConfigProto(
+        allow_soft_placement=True, log_device_placement=True
+    )
+    config.gpu_options.allow_growth = True
+
+    config = (
+        tf.estimator.RunConfig(save_checkpoints_steps=params["checkpoints"])
+        .replace(save_summary_steps=1)
+        .replace(session_config=config)
+    )
+
+    # Generate estimator object
+    estimator = tf.estimator.Estimator(
+        model_fn=model_fn,
+        model_dir=params["log_dir"],
+        params=params,
+        config=config,
+    )
+
+    if params["mode"] == "train":
+        input_fn = partial(input_fn_gen, mode='train', params='params')
+        train_spec = tf.estimator.TrainSpec(input_fn=input_fn)
+        estimator.train(input_fn)
+
+    elif params["mode"] == "train_eval":
+
+        train_spec = tf.estimator.TrainSpec(input_fn=input_fn)
+        eval_spec = tf.estimator.EvalSpec(
+            input_fn=input_eval, steps=700, throttle_secs=10
+        )
+        tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+
+    elif params["mode"] == "eval":
+        eval_spec = tf.estimator.EvalSpec(
+            input_fn=input_eval, steps=700, throttle_secs=10
+        )
+        estimator.evaluate(input_eval)
+
