33fa64c385d2f44bfe28bfa7d1ee4020be674ae4
sally14 <salome.do@ensae.fr>
2019-05-25 05:05:15 +0200

    Added train.sh

--- train.sh
+++ train.sh
@@ -8,7 +8,27 @@ python train.py taoteba  logs/logs_taoteba_LSTM_Dense \
              --n_epochs=10 \
              --batch_size=256
 
+python train.py wiki  logs/logs_wiki_LSTM_Dense \
+             --optimizer=adam \
+             --learning_rate=0.001 \
+             --add_char_emb=True \
+             --weighted_loss=False \
+             --add_encoder=False \
+             --deepness_finish=3 \
+             --n_epochs=10 \
+             --batch_size=256
+
 python train.py taoteba  logs/logs_taoteba_LSTM \
+             --optimizer=adam \
+             --learning_rate=0.001 \
+             --add_char_emb=True \
+             --weighted_loss=False \
+             --add_encoder=False \
+             --deepness_finish=0 \
+             --n_epochs=10 \
+             --batch_size=256
+
+python train.py wiki  logs/logs_wiki_LSTM \
              --optimizer=adam \
              --learning_rate=0.001 \
              --add_char_emb=True \
@@ -28,7 +48,17 @@ python train.py taoteba  logs/logs_taoteba_Encoder \
              --n_epochs=10 \
              --batch_size=256
 
-python train.py taoteba  logs/logs_taoteba_Encoder_no_chars \
+python train.py wiki  logs/logs_wiki_Encoder \
+             --optimizer=adam \
+             --learning_rate=0.001 \
+             --add_char_emb=True \
+             --weighted_loss=False \
+             --add_encoder=True \
+             --deepness_finish=0 \
+             --n_epochs=10 \
+             --batch_size=256
+
+python train.py wiki  logs/logs_taoteba_Encoder_no_chars \
              --optimizer=adam \
              --learning_rate=0.001 \
              --add_char_emb=False \
@@ -50,13 +80,3 @@ python train.py taoteba  logs/logs_taoteba_LSTM_chars_weighted_loss \
 
 
 
-python train.py taoteba  logs/logs_taoteba_LSTM_test_num_layers \
-             --optimizer=rmsprop \
-             --learning_rate=0.001 \
-             --add_char_emb=False \
-             --weighted_loss=False \
-             --add_encoder=False \
-             --deepness_finish=2 \
-             --n_epochs=1 \
-             --batch_size=256 \
-             --num_LSTM_layers=15
