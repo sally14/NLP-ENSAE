4f01b584ae2ac7ce57b8f40ec19a462c0b1aa364
sally14 <salome.do@ensae.fr>
2019-05-25 03:14:35 +0200

    Repaired weighted loss

--- train.sh
+++ train.sh
@@ -1,1 +1,5 @@
-python train.py taoteba  logs/logs_taoteba_LSTM  --learning_rate=10 --add_char_emb=False --weighted_loss=False --add_encoder=False --deepness_finish=0 --n_epochs=1 --batch_size=256
+python train.py taoteba  logs/logs_taoteba_LSTM  --learning_rate=0.01 --add_char_emb=False --weighted_loss=False --add_encoder=False --deepness_finish=0 --n_epochs=1 --batch_size=256
+
+
+
+python train.py taoteba  logs/logs_taoteba_weight  --learning_rate=0.1 --add_char_emb=False --weighted_loss=True --add_encoder=False --deepness_finish=0 --n_epochs=1 --batch_size=256
