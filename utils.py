e59fb2a1bdbd95649ab17a3d89cd0fa83ed1ecd4
sally14 <salome.do@ensae.fr>
2019-05-25 15:54:04 +0200

    typo

--- utils.py
+++ utils.py
@@ -116,8 +116,8 @@ def write_ngrams(write_path, basename, sents, n=5):
     sent_basename = os.path.join(write_path, basename + ".sents")
     label_basename = os.path.join(write_path, basename + ".labels")
     # Now that we have sents, mask n random words / generate lm model
-    with open(sent_basename, "a+", encoding="utf-8") as s_write:
-        with open(label_basename, "a+", encoding="utf-8") as l_write:
+    with open(sent_basename, "a", encoding="utf-8") as s_write:
+        with open(label_basename, "a", encoding="utf-8") as l_write:
             for i in range(len(n_grams)-1):
                 s_write.write(n_grams[i] + '\n')
                 l_write.write(n_grams[i+1].split(' ')[-1]+'\n')
